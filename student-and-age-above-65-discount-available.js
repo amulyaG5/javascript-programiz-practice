02145862132f92d4082da491b0296383c5ae8655
Amulya Prabhu <amulya.prabhu@galleri5.com>
2023-06-20 15:57:07 +0530

    student-and-age-above-65-discount-available.js

---
+++ student-and-age-above-65-discount-available.js
@@ -0,0 +1,10 @@
+
+let isStudent = true
+let age = 5
+
+if (isStudent || age >= 65) {
+    console.log("You are eligible for a discount");
+}
+else {
+    console.log("You are not eligible for a discount")
+}

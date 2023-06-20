0c1e4f7e52406e859b6cbfc7029576349b487c9a
Amulya Prabhu <amulya.prabhu@galleri5.com>
2023-06-20 15:15:04 +0530

    age question for voting eliblity

---
+++ person-elgibility-for-vote.js
@@ -0,0 +1,6 @@
+let n = 20
+if (n >= 18) {
+    console.log("You are eligible to vote")
+} else {
+    console.log("You are not eligible to vote yet")
+}
---
+++ prime-number.js
@@ -0,0 +1,4 @@
+// 1 is considered neither prime nor composite.
+// All negative numbers are excluded because prime numbers are positive.
+// // Numbers greater than 1 are tested using a for loop.
+
